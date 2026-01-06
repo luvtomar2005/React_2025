1313e509e4ce64de318e066429b231954c3c9caa
luvtomar2005 <luvtomar2005@gmail.com>
2026-01-07 01:43:15 +0530

    Added the tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./Lecture 10/index.html",
+    "./Lecture 10/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
